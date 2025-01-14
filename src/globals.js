bd51285cf41a0da44f92afe0b394dea4681e916a
ALEXA8596 <alexjunyoung+github@gmail.com>
2025-01-13 19:13:06 -0800

    Fixed errors

--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,4 @@
 window.Buffer = window.Buffer || require('buffer').Buffer;
+import process from 'process/browser';
+global.process = process;
+window.process=  process;
--- src/globals.native.js
+++ src/globals.native.js
@@ -1,3 +1,6 @@
 import 'react-native-url-polyfill/auto';
 import 'react-native-get-random-values';
+import 'process/browser'
 global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = require('process/browser');
+// console.log(global.process)
